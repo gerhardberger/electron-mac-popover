a679a2e88763bab78407271c4393e6a254e9d6f5
David Cann <david@davidcann.com>
2024-12-13 14:07:54 +0700

    TypeScript types

---
+++ types.d.ts
@@ -0,0 +1,33 @@
+declare module 'electron-mac-popover' {
+  export interface ShowOptions {
+    rect: {
+      x: number;
+      y: number;
+      width: number;
+      height: number;
+    };
+    size: {
+      width: number;
+      height: number;
+    };
+    edge?: 'max-x-edge'
+      | 'max-y-edge'
+      | 'min-x-edge'
+      | 'min-y-edge';
+	behavior?: 'transient' | 'semi-transient' | 'application-defined';
+    animates?: boolean;
+    appearance?: 'aqua'
+      | 'darkAqua'
+      | 'vibrantLight'
+      | 'accessibilityHighContrastAqua'
+      | 'accessibilityHighContrastDarkAqua'
+      | 'accessibilityHighContrastVibrantLight'
+      | 'accessibilityHighContrastVibrantDark';
+  }
+
+  export class ElectronMacPopover {
+    constructor(nativeWindowHandle: Buffer);
+    show(nativeWindowHandle: Buffer, options: ShowOptions): void;
+    close(): void;
+  }
+}
